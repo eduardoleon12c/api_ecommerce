c239688d5068a694a5ab7b6498131e968c6af031
EduardoLeonCastillo <eduardoleon1266@gmail.com>
2024-01-29 19:12:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import mongoose from 'mongoose'
+import router from './router'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+//CONEXION A LA BASE DE DATOS
+
+mongoose.Promise = global.Promise;
+const dbUrL = "mongodb://127.0.0.1:27017/ecommerce_jaknet";
+mongoose.connect(
+    dbUrL , {
+        useNewUrlParser:  true,
+        useUnifiedTopology: true
+    }
+).then(mongoose => console.log("CONECTADO A LA BD EN EL PUERTO 27017"))
+.catch(err => console.log(err));
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/api/',router)
+
+app.set('port',process.env.PORT || 4000);
+
+app.listen(app.get('port'), () => {
+    console.log("EL SERVIDOR SE EJECUTO PERFECTAMENTE EN EL PUERTO 4000");
+})
